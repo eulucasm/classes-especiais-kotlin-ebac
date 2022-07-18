030520bed4fe8583f2e6158705fe4920f4e863a6
Lucas <Lucas.Marques-ext@ab-Inbev.com>
2022-07-18 20:31:18 -0300

    Companion object

--- app/src/main/java/br/com/lucao/ebac/classesespeciais/aula/CompanionObject.kt
+++ app/src/main/java/br/com/lucao/ebac/classesespeciais/aula/CompanionObject.kt
@@ -1,3 +1,44 @@
 package br.com.lucao.ebac.classesespeciais.aula
 
 class CompanionObject {}
+
+fun main() {
+    val person = Persons.Manager.createPerson()
+    person.name = "Lucas"
+
+    println(Persons.counter)
+    println(Persons.counter)
+    println(Persons.counter)
+    println(person.getTotalPerson())
+    person.helloPerson()
+    println(Persons.decrementCounter())
+
+
+}
+
+fun Persons.helloPerson(){
+    println("Olá ${this.name}")
+}
+
+fun Persons.Manager.decrementCounter(){
+    counter--
+    println(counter)
+}
+
+class Persons private constructor() {
+    var name = ""
+    var email = ""
+
+    fun getTotalPerson(): Int = counter
+
+    companion object Manager {
+        var counter = 0
+
+        fun createPerson(): Persons {
+            var persons = Persons()
+            counter++
+
+            return Persons()
+        }
+    }
+}
